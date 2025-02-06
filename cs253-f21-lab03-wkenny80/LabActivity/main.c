dc7eef2ee1204dde7059189ddf1c7d15bb858175
William Kenny <61764458+wkenny80@users.noreply.github.com>
2025-02-06 10:09:59 -0800

    Add files via upload

---
+++ cs253-f21-lab03-wkenny80/LabActivity/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>                  // Note: Needed for math functions in part (3)
+
+int main(void) {
+
+// FIXME (1): Prompt user to input wall's width
+
+   
+   // Calculate and output wall area
+
+   
+   // FIXME (2): Calculate and output the amount of paint in gallons needed to paint the wall
+
+
+   // FIXME (3): Calculate and output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
+
+   return 0;
+}
